0cd09d05784c40b43d5dbcaee40b3c6163511d0d
veersen2001 <veerbahadursen@gmail.com>
2024-04-27 10:50:48 +0530

    backend_1

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 // Third-Party
 app.use(
   cors({
-    origin:'http://localhost:5173', //5000
+    origin:'https://lms-frontend-xi-swart.vercel.app', //5000
     credentials: true,
   })
 )
