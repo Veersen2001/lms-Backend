b199aae70b2711d93a3bbca40bfbcb981a4ed867
veersen2001 <veerbahadursen@gmail.com>
2024-04-29 21:19:28 +0530

    ra

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ export const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_SECRET,
 });
-
+ console.log(razorpay);
 const PORT = process.env.PORT || 5000;
 
 
