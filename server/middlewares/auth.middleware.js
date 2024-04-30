9058741113e57ad73ec3922ab4036239cb9dfc59
veersen2001 <veerbahadursen@gmail.com>
2024-04-30 16:09:15 +0530

    rt

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -3,11 +3,10 @@ import jwt from "jsonwebtoken";
 import AppError from "../utils/appError.js";
 import asyncHandler from "./asyncHandler.middleware.js";
 
-export const isLoggedIn = async (req, _res, next) => {
+export const isLoggedIn = asyncHandler(async (req, _res, next) => {
   // extracting token from the cookies
   const { token } = req.cookies;
   console.log("Token"+token);
-  console.log("req"+req.cookies);
 
   // If no token send unauthorized message
   if (!token) {
@@ -27,7 +26,7 @@ export const isLoggedIn = async (req, _res, next) => {
 
   // Do not forget to call the next other wise the flow of execution will not be passed further
   next();
-};
+});
 
 // Middleware to check if user is admin or not
 export const authorizeRoles = (...roles) =>
