32d93678f8d0a0761c4b76e5600ce9c1808076e6
veersen2001 <veerbahadursen@gmail.com>
2024-04-30 22:33:00 +0530

    front

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -12,7 +12,7 @@ const cookieOptions = {
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
    path: "/",
    httpOnly: true,
-        sameSite: "none",
+        SameSite: "none",
         secure: true,
         domain: "https://lms-frontend-qmln.vercel.app",
 
