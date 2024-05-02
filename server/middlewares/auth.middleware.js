eab704808aabe1e820edc14e7e3295e3a5b7c977
veersen2001 <veerbahadursen@gmail.com>
2024-05-02 14:38:54 +0530

    o

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -4,12 +4,12 @@ import AppError from "../utils/appError.js";
 import asyncHandler from "./asyncHandler.middleware.js";
 
 export const isLoggedIn = asyncHandler(async (req, _res, next) => {
-  const { email, password } = req.body;
+  const email = req.body;
 
   // Check if the data is there or not, if not throw error message
   
   // extracting token from the cookies
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email });
   // const token  = req.cookies;
   const token = await user.generateJWTToken();
   console.log("Token"+token);
