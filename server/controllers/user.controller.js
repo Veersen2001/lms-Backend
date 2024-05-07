098707d248d3821a9c042117b4d2e01291373a8e
veersen2001 <veerbahadursen@gmail.com>
2024-05-07 20:41:14 +0530

    commit

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -146,7 +146,7 @@ const token = await user.generateJWTToken();
 user.password = undefined;
 
 // Setting the Set-Cookie header with SameSite=None
-const cookieValue = `token=${token}; HttpOnly; Secure;SameSite:None; Path=/`;
+const cookieValue = `token=${token}; HttpOnly; Secure;SameSite:None; Path=/api/v1/`;
 res.setHeader('Set-Cookie', cookieValue);
 
 // Setting the token in the cookie with name 'token' along with cookieOptions
