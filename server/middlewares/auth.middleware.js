92850db6a6b57fab69bf0b3b812253da8667fe2b
veersen2001 <veerbahadursen@gmail.com>
2024-05-07 13:04:17 +0530

    commit

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ export const isLoggedIn = asyncHandler(async(req, _res, next) => {
   
    const {token} = req.cookies;
     console.log('Cookies:: ', req.cookies)
-    _res.send(); 
+   
   //  const user = await User.findOne({ email })
   //  const token = await user.generateJWTToken();
  
